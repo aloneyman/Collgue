dfa93817ae9316474b64cc6368ae54b92df20eaa
aloneyman <1246465494@qq.com>
2021-09-30 20:20:25 +0800

    Initial Commit

---
+++ Demo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Demo
+//
+//  Created by 蔡洪浩 on 2021/9/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
