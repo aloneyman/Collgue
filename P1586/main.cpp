ce965ed359373ec6226b943af0c84c64d30ecfe3
chh <caihonghao@gmail.com>
2021-10-18 19:09:14 +0800

    asd

---
+++ P1586/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  P1586
+//
+//  Created by 蔡洪浩 on 2021/10/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
